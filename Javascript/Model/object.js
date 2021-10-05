15f0ec65d56189765fb589edf0b0a136618485ce
mihneacojocaru <mihnea.co@gmail.com>
2021-10-05 20:50:36 +0200

    first commit

---
+++ Javascript/Model/object.js
@@ -0,0 +1,9 @@
+class Product{
+
+    constructor(){
+
+    }
+
+}
+
+export default Product;
---
+++ Javascript/app.js
@@ -0,0 +1,5 @@
+import Product from "./Model/object.js";
+import Controller from "./Controller/objectController.js";
+
+
+let product = new Product();
